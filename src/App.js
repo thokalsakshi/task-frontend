4c4143e23a1490d4deffdb12d0b16fc9303f9463
PruthviThokal <thokalpruthviraj99@gmail.com>
2024-03-26 19:48:49 +0530

    add structure for frontend

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-
 const App = () => {
   return (
     <div className="App">
